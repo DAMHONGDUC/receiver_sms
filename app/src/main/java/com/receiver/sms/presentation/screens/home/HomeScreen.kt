b6545735c07e5abd9d66d939153373de9fdb26c4
Dam Hong Duc <hongduc001h@gmail.com>
2024-08-20 10:33:03 +0700

    feat: add app container

--- app/src/main/java/com/receiver/sms/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/receiver/sms/presentation/screens/home/HomeScreen.kt
@@ -3,38 +3,22 @@ package com.receiver.sms.presentation.screens.home
 import android.annotation.SuppressLint
 import android.content.Context
 import android.net.Uri
-import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import com.receiver.sms.presentation.components.app_container.AppContainer
 
 private val LOG_TAG = "HomeScreenLOG"
 
 @Composable
 fun HomeScreen() {
-    val context = LocalContext.current
-    val smsList = remember { mutableStateOf(listOf<SmsMessage>()) }
-
-    LaunchedEffect(Unit) {
-        smsList.value = getSmsList(context)
-        Log.d(LOG_TAG, smsList.value.toString())
-    }
-
     Scaffold { paddingValues ->
-        LazyColumn {
-            items(smsList.value) { sms ->
-                SmsItem(sms)
-            }
+        AppContainer {
+            Text(text = "HomeScreen")
         }
     }
 }
