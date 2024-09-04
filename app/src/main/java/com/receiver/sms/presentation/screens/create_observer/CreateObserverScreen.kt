678cfeb2442ba2f5fd5fa6676c37e9ea2fca054a
Dam Hong Duc <hongduc001h@gmail.com>
2024-09-04 13:36:04 +0700

    feat: fix build

--- app/src/main/java/com/receiver/sms/presentation/screens/create_observer/CreateObserverScreen.kt
+++ app/src/main/java/com/receiver/sms/presentation/screens/create_observer/CreateObserverScreen.kt
@@ -14,45 +14,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import com.receiver.sms.presentation.screens.main.MainViewModel
-import tech.devscast.validable.EmailValidable
-import tech.devscast.validable.core.rememberValidator
 
 @Composable
 fun CreateObserverScreen(mainVM: MainViewModel) {
-    val emailField = remember { EmailValidable("aaaaaa") }
-    // pass all fields to the withValidable method
-    val validator = rememberValidator(emailField)
-    val context = LocalContext.current
-
-    Column {
-        TextField(
-            value = emailField.value,
-            onValueChange = { emailField.value = it }, // update the text
-            isError = emailField.hasError(), // check if the field is not valid
-        )
-
-        AnimatedVisibility(visible = emailField.hasError()) {
-            Text(
-                text = emailField.errorMessage ?: "",
-                modifier = Modifier.fillMaxWidth(),
-                style = LocalTextStyle.current.copy(color = MaterialTheme.colors.error)
-            )
-        }
-
-        Button(
-            // a state to check if all fields are valid, without submitting the form
-            enabled = validator.isValid,
-            onClick = {
-                validator.validate {
-                    // will be executed if all fields are valid
-                    Toast.makeText(context, "All fields are valid", Toast.LENGTH_SHORT)
-                        .show()
-                }
-            }
-        ) {
-            Text(text = "Submit")
-        }
-    }
 
 }
 
