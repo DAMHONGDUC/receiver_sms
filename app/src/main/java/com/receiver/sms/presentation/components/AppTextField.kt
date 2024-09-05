791324112c3314c8105ffe397f24444be98b6119
Dam Hong Duc <hongduc001h@gmail.com>
2024-09-05 22:50:10 +0700

    feat: custom app text field

--- app/src/main/java/com/receiver/sms/presentation/components/AppTextField.kt
+++ app/src/main/java/com/receiver/sms/presentation/components/AppTextField.kt
@@ -2,17 +2,25 @@ package com.receiver.sms.presentation.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.Dp
@@ -20,6 +28,7 @@ import androidx.compose.ui.unit.dp
 import com.receiver.sms.presentation.components.spacing.VerticalSpacing
 import com.receiver.sms.utils.resources.AppBoxModel
 import com.receiver.sms.utils.resources.AppColors
+import com.receiver.sms.utils.resources.AppIconSize
 import com.receiver.sms.utils.resources.AppTextStyle
 
 @Composable
@@ -50,27 +59,58 @@ fun AppTextField(
                 .background(AppColors.textFieldBG, shape = RoundedCornerShape(borderRadius))
                 .padding(AppBoxModel().mainPadding())
         ) {
-            BasicTextField(
+            Row(
                 modifier = Modifier.fillMaxWidth(),
-                value = value,
-                onValueChange = onValueChange,
-                textStyle = mainTextStyle,
-                decorationBox = { innerTextField ->
-                    if (value.isEmpty()) {
-                        Text(
-                            text = placeholder,
-                            style = mainTextStyle
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                leadingIcon?.invoke()
+
+                BasicTextField(
+                    modifier = Modifier.weight(1f),
+                    value = value,
+                    onValueChange = onValueChange,
+                    textStyle = mainTextStyle,
+                    decorationBox = { innerTextField ->
+                        if (value.isEmpty()) {
+                            Text(
+                                text = placeholder,
+                                style = mainTextStyle.copy(color = AppColors.textFieldPH)
+                            )
+                        }
+                        innerTextField()
+                    },
+                    keyboardOptions = keyboardOptions.copy(
+                        imeAction = ImeAction.Next
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onNext = { onNext?.invoke() }
+                    )
+                )
+
+                if (value.isNotEmpty()) {
+                    Box(
+                        modifier = Modifier
+                            .size(AppIconSize().small())
+                            .clip(RoundedCornerShape(50))
+                            .background(AppColors.gray)
+                            .clickable {
+                                onValueChange("")
+                            }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Close,
+                            contentDescription = "Clear text",
+                            tint = AppColors.white,
+                            modifier = Modifier
+                                .size(AppIconSize().base())
+                                .padding(3.dp)
                         )
                     }
-                    innerTextField()
-                },
-                keyboardOptions = keyboardOptions.copy(
-                    imeAction = ImeAction.Next
-                ),
-                keyboardActions = KeyboardActions(
-                    onNext = { onNext?.invoke() }
-                )
-            )
+
+                } else {
+                    trailingIcon?.invoke()
+                }
+            }
         }
         if (isError && !errorMessage.isNullOrEmpty()) {
             Text(
