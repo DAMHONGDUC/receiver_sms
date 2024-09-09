84c80d3482749a1d751d7fb6e0749c15348e95e6
Dam Hong Duc <hongduc001h@gmail.com>
2024-09-09 21:27:08 +0700

    feat: rm network inject

--- app/src/main/java/com/receiver/sms/core/di/RepositoryModule.kt
+++ app/src/main/java/com/receiver/sms/core/di/RepositoryModule.kt
@@ -1,7 +1,6 @@
 package com.receiver.sms.core.di
 
 import com.receiver.sms.data.data_source.local.LocalDatabase
-import com.receiver.sms.data.data_source.remote.APIService
 import com.receiver.sms.data.repository.APIRepository
 import com.receiver.sms.data.repository.DBRepository
 import com.receiver.sms.domain.use_case.CallAPIAfterReceiveSMSUC
@@ -22,12 +21,6 @@ class RepositoryModule {
     @Singleton
     fun provideDBRepository(db: LocalDatabase): DBRepository {
         return DBRepository(dao = db.dao)
-    }
-
-    @Provides
-    @Singleton
-    fun provideAPIRepository(apiService: APIService): APIRepository {
-        return APIRepository(apiService = apiService)
     }
 
     @Provides
--- app/src/main/java/com/receiver/sms/data/repository/APIRepository.kt
+++ app/src/main/java/com/receiver/sms/data/repository/APIRepository.kt
@@ -1,7 +1,6 @@
 package com.receiver.sms.data.repository
 
 import android.util.Log
-import com.receiver.sms.data.data_source.remote.APIService
 import com.receiver.sms.data.data_source.remote.IAPIDataSource
 import com.receiver.sms.domain.model.SMSObserveModel
 import javax.inject.Inject
@@ -9,10 +8,9 @@ import javax.inject.Inject
 private val LOG_TAG = "APIRepositoryLOG"
 
 class APIRepository @Inject constructor(
-    private val apiService: APIService,
 ) : IAPIDataSource {
     override suspend fun callAPIAfterReceiveSMS(smsObserveModel: SMSObserveModel) {
         Log.d(LOG_TAG, smsObserveModel.toString())
-        apiService.callAPIAfterReceiveSMS(smsObserveModel)
+//        apiService.callAPIAfterReceiveSMS(smsObserveModel)
     }
 }
