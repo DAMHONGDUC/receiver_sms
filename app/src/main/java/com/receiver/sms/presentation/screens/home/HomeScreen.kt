8e8b5e5d8333676915554f49b68ef16db7f1f92f
Dam Hong Duc <hongduc001h@gmail.com>
2024-09-09 09:30:54 +0700

    feat: improve create observer button

--- app/src/main/java/com/receiver/sms/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/receiver/sms/presentation/screens/home/HomeScreen.kt
@@ -1,18 +1,28 @@
 package com.receiver.sms.presentation.screens.home
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.receiver.sms.presentation.components.CustomAppBar
 import com.receiver.sms.presentation.components.MainContainer
+import com.receiver.sms.presentation.components.Pressable
 import com.receiver.sms.presentation.screens.home.components.CreateSMSObserverButton
 import com.receiver.sms.presentation.screens.home.components.ListSMSObserver
 import com.receiver.sms.presentation.screens.home.components.RequestSMSPermissionModal
 import com.receiver.sms.presentation.screens.main.MainViewModel
 import com.receiver.sms.utils.resources.AppColors
+import com.receiver.sms.utils.resources.AppIconSize
+import com.receiver.sms.utils.resources.AppTextStyle
 
 private val LOG_TAG = "HomeScreenLOG"
 
@@ -37,9 +47,22 @@ fun HomeScreen(
             CustomAppBar(
                 bg = AppColors.primary,
                 mainColor = AppColors.white,
-                title = "Home",
+                title = "SMS Observer",
+                textStyle = AppTextStyle().base.copy(fontWeight = FontWeight.SemiBold),
                 enableBack = false,
-                elevation = 0.dp
+                elevation = 0.dp,
+                topBarAction = {
+                    Pressable(onClick = { /*TODO*/ }) {
+                        Icon(
+                            Icons.Filled.Search,
+                            contentDescription = "",
+                            tint = AppColors.white,
+                            modifier = Modifier
+                                .padding(5.dp)
+                                .size(AppIconSize().base())
+                        )
+                    }
+                }
             )
         }
     ) {
